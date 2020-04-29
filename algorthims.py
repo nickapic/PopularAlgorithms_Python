e141981bf9dd69d553d178994a533a4cfd121d4e
Aniket Chauhan <43927184+nickapic@users.noreply.github.com>
2020-04-29 13:00:10 +0300

    New Algorithms added

--- algorthims.py
+++ algorthims.py
@@ -87,3 +87,27 @@ def selectionSort(arr):
 
 print("Selection Sort here : ")
 print(selectionSort([5, 3, 6, 2, 10]))
+
+
+# Recursion is basically the act of dividing the function input into asmany smaller bits as you can divide it and call the same function on it .This doesnt give any time coplexity advantages but its just a good way to implement and makes the code more clearer
+# But Because its repeating the same thing again and again and dividing stuff into smaller taks its common to face Infinite Loop Issues if you dont have a base case we will run into a infinite Loop here the base case is when the array has less than 2 so like 0 or 1 member
+# The implementation below is using recursion and of the algortihm called quicksort which has time complexity of O(nlogn) which is kinda slow but for sorting its preety good it should be used instead of Selection sort.
+def quicksort(array):
+    if(len(array) < 2):
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+# Important Point- Recursion uses a data dtructure called Stacks which basically means that the the element that is intered last would be the first go out .Imagine it like  stack of books and to get the last one you keep taking the book on the top position till you reach the last book
+print("Quick Sort Result implemneted with Recursion")
+largelist = [45, 243, 23, 2, 1, 5, 6, 89, 20]
+print(quicksort(largelist))
+
+# Hash Tables ( Very Important Method for Problems )
+#In Simpler words hash tables are basically like a phonebook of sorts you have a list of names (keys) and then the phone numbers[values] so as long as you know what the name of the person is its constant time.
+# In python a dict is basically a hash table so it contains a key and a value for that key .To get data out of a hash table is constant time complexity O(1) [So the best possible one] .
+#For Example I will put the Solution for a Interview Question From Google the problem
